3660e90a19e82b753d48b3ea1d0482df1189b153
valenciavguides <55606798+valenciavguides@users.noreply.github.com>
2025-10-13 18:12:30 +0200

    Update server.js

--- js/server.js
+++ js/server.js
@@ -3,6 +3,19 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware para agregar headers de seguridad
+app.use((req, res, next) => {
+    // Configurar Permissions-Policy para permitir geolocalización y NO incluir 'unload'
+    res.setHeader('Permissions-Policy', 'geolocation=(self), interest-cohort=()');
+    
+    // Agregar headers de seguridad adicionales
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    
+    next();
+});
+
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, '../')));
 
